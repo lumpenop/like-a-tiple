b35f9ce60c1c2368d8c0abb6cdcfe85c1d195c99
lumpenop <swk5373@gmail.com>
2022-07-01 03:43:01 +0900

    Add: GlobalStyle 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Awards from 'components/Awards'
 
-function App() {
+import { GlobalStyle } from './styles/global'
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="App">
+      <GlobalStyle />
+      <Awards />
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  section {
+    box-sizing: border-box;
+    color: #393939;
+  }
+`
